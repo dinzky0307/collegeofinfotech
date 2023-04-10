e448c50f505ad7b3ee257d5837a02d9231765ea4
Jantinn Erezo <erezojantinn@gmail.com>
2023-04-10 17:34:05 +0800

    Update connection.php

--- teacher/connection.php
+++ teacher/connection.php
@@ -3,7 +3,7 @@
 define('DB_SERVER', '127.0.0.1');
 define('DB_USERNAME', 'infotechmcc');
 define('DB_PASSWORD', 'infotechmcc');
-define('DB_NAME', 'infotech');
+define('DB_NAME', 'infotechmcc');
 $dbconnection = mysqli_connect(DB_SERVER, DB_USERNAME, DB_PASSWORD, DB_NAME);
 if($dbconnection === false){
     die("ERROR: Could not connect. " . mysqli_connect_error());
