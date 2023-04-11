5050c73b615b4c8b567bf0ca3cacb8bff336e88c
Jobert Chavez <130432448+JobertChavez@users.noreply.github.com>
2023-04-11 16:21:34 +0800

    Update form.php

--- student/form.php
+++ student/form.php
@@ -50,6 +50,9 @@
     <script defer src="https://unpkg.com/alpinejs@3.x.x/dist/cdn.min.js"></script>
 
     <script src="//cdn.jsdelivr.net/npm/sweetalert2@11"></script>
+    <link rel="stylesheet" href="//code.jquery.com/ui/1.12.1/themes/base/jquery-ui.css">
+    <script src="https://code.jquery.com/jquery-1.12.4.js"></script>
+    <script src="https://code.jquery.com/ui/1.12.1/jquery-ui.js"></script>
       <script>
 	  		$( function() {
 	   			$( "#date" ).datepicker({
