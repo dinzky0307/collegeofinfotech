1672b2fd81ead2030c14f87ba59f8925239a1fc2
Jobert Chavez <130432448+JobertChavez@users.noreply.github.com>
2023-05-01 16:00:31 +0800

    Update updategrade.php

--- teacher/updategrade.php
+++ teacher/updategrade.php
@@ -3,13 +3,13 @@
 include 'connection.php';
 
 $id = $_GET['id'];
-$pre = $_GET['p'];
 $midterm = $_GET['m'];
 $final = $_GET['f'];
 $classid = $_GET['c'];
+$total = ($midterm + $final) / 2;
 
 
-$sql = "UPDATE studentsubject SET prelim_grade='$pre', midterm_grade='$midterm', final_grade='$final' WHERE studid='$id' AND classid='$classid'";
+$sql = "UPDATE studentsubject SET midterm_grade='$midterm', final_grade='$final', total='$total' WHERE studid='$id' AND classid='$classid'";
 
 if ($dbconnection->query($sql) === TRUE) {
 	echo "<script>window.location.href='student.php?classid=".$classid."'</script>";
