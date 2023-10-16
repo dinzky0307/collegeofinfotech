b726c55cd8baff7d339a0f80992d4b9965029057
Ryyan <132124759+Raiden-source@users.noreply.github.com>
2023-10-16 18:48:38 +0800

    Update set_session.php

--- admin/set_session.php
+++ admin/set_session.php
@@ -1,1 +1,20 @@
+<?php
+
+if (isset($_POST['id'])) {
+    $studentId = $_POST['id'];
+
+    // Assuming you have a database connection and a students table
+    // Fetch the student details from the database based on the ID
+    // For example:
+    // $student = fetchStudentById($studentId);
+
+    // Set the session ID based on the student's ID or any other identifier
+    $_SESSION['id'] = $studentId;
+
+    // Return a response if needed (optional)
+    echo json_encode(['success' => true]);
+} else {
+    echo json_encode(['success' => false, 'error' => 'Invalid student ID']);
+}
+?>
 
