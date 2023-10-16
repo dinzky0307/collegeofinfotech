d7e46f4d6df4a7f696c7bf48f817b82c7baa076a
Ryyan <132124759+Raiden-source@users.noreply.github.com>
2023-10-16 23:08:36 +0800

    Update forgot-password.php

--- forgot-password.php
+++ forgot-password.php
@@ -1,19 +1,69 @@
 <?php
 include 'config.php';
+use PHPMailer\PHPMailer\PHPMailer;
+use PHPMailer\PHPMailer\Exception;
+use PHPMailer\PHPMailer\SMTP;
+
+require_once("phpmailer/src/Exception.php");
+require_once("phpmailer/src/PHPMailer.php");
+require_once("phpmailer/src/SMTP.php");
 
 if (isset($_POST['submit'])) {
-    $user = $_POST['user'];
+    $user = $_POST['email'];
     
     // Check if the provided ID number exists in the userdata table
-    $query = "SELECT * FROM userdata WHERE username='$user'";
+    $query = "SELECT * FROM teacher WHERE email = '$user' ";
     $result = mysql_query($query);
     
     if ($result && mysql_num_rows($result) == 1) {
-        header('location: reset-pass.php');
-        $_SESSION['username'] = $user;
+        $data = mysql_fetch_assoc($result);
+        $verification = uniqid(rand(2,5));
+        $teach_id = $data['teachid'];
+        $email = $_POST['email'];
+
+        $mail = new PHPMailer(true);
+
+        $mail->SMTPDebug = 0; // Enable verbose debug output
+        $mail->isSMTP(); // Send using SMTP
+        $mail->Host = 'smtp.gmail.com'; // Set the SMTP server to send through
+        $mail->SMTPAuth = true; // Enable SMTP authentication
+        $mail->Username = 'enelyntribunalo@gmail.com'; // SMTP username
+        $mail->Password = 'tcuuujxrlvbxvdxn'; // SMTP password
+        $mail->SMTPSecure = 'tls'; // Enable TLS encryption; `PHPMailer::ENCRYPTION_SMTPS` also accepted
+        $mail->Port = 587; // TCP port to connect to
+
+        $mail->SMTPOptions = array(
+            'ssl' => array(
+                'verify_peer' => false,
+                'verify_peer_name' => false,
+                'allow_self_signed' => true
+            )
+        );
+
+        $mail->setFrom('enelyntribunalo@gmail.com', 'MCC Info Tech');
+        $mail->addAddress($email);
+        
+    
+        $session = $_SESSION['reset'] = $verification; 
+    
+        //Content
+        $mail->isHTML(true);                                  //Set email format to HTML
+        $mail->Subject = 'Reset Password';
+        $mail->Body    = "Click the link to reset password : <a href='http://localhost/infotech/reset-pass.php?reset=$teach_id&id=$session'>Click here</a>";
+    
+
+        $mail->send();
+
+        ?>
+        <script>
+            alert("Please check your email account for confirmation")
+            window.location.href = "forgot-password.php"
+        </script>
+        <?php 
+
     } else {
         // User does not have an account, display an error message
-        $errorMessage = "No account found with the provided ID number.";
+        $errorMessage = "No account found with the provided Email account.";
     }
 }
 ?>
@@ -47,7 +97,7 @@ if (isset($_POST['submit'])) {
                     </div>
                     <div class="input-field">
                         <i class="fas fa-user"></i>
-                        <input type="text" placeholder="ID Number" name="user"  required />
+                        <input type="text" placeholder="Email" name="email"  required />
                     </div>
                     <input type="submit" value="Submit" name="submit" class="btn solid" />
                     <button type="button" class="btn cancel" id="cancelButton" style="height: 44px;">Cancel</button>
@@ -80,4 +130,3 @@ if (isset($_POST['submit'])) {
     <script src="app.js"></script>
 </body>
 </html>
-
