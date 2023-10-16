d2f77535eda0c424a4ba8c3e80675f3f15e063f6
Ryyan <132124759+Raiden-source@users.noreply.github.com>
2023-10-16 17:48:55 +0800

    Update messages_model.php

--- admin/data/messages_model.php
+++ admin/data/messages_model.php
@@ -1,63 +1,89 @@
-<?php    
+<?php
+    $settings = new Datasettings();
+    if(isset($_GET['q'])){
+        $settings->$_GET['q']();
+    }
+    
+    class Datasettings {
+        
+        function __construct(){
+            if(!isset($_SESSION['id'])){
+                header('location:../../');   
+            }
+        }
+        
+         //create logs
+        function logs($act){            
+            $date = date('m-d-Y h:i:s A');
+            echo $q = "insert into log values(null,'$date','$act')";   
+            mysql_query($q);
+            return true;
+        }
+        
+        function changepassword(){
+            include('../../config.php');
+            $username = $_GET['username'];
+            $new = sha1($_POST['new']);
+            $confirm = sha1($_POST['confirm']);
+            if($new == $confirm){
+                $r2 = mysql_query("update userdata set password='$new' where username='$username'");
+                header('location:../settings.php?msg=success&username='.$username.'');   
+            }else{
+                header('location:../settings.php?msg=error&username='.$username.'');   
+            }   
+            
+            $act = "update password of username $username";
+            $this->logs($act);
+        }
+        
+        function addaccount(){
+            include('../../config.php');
+            $level = $_GET['level'];
+            $id = $_GET['id'];
+            $q = "select * from $level where id=$id";
+            $r = mysql_query($q);
+            $row = mysql_fetch_array($r);
+            if($level == 'student'){
+                $username = $row['studid'];                
+                $fname = $row['fname'];
+                $lname = $row['lname'];
+                $password = $username;
+            }else{
+                $username = $row['teachid'];                
+                $fname = $row['fname'];
+                $lname = $row['lname'];
+                $password = $username;
+            }
+            $verify = $this->verifyusername($username);
+            if($verify){
+                $q2 = "insert into userdata values(null,'$username','$password','$fname','$lname','$level')";
+                mysql_query($q2);
+                header('location:../'.$level.'list.php?r=added an account');
+            }else{
+                  header('location:../'.$level.'list.php?r=has already an account'); 
+            }
+            
+            $act = "add account with the username of $username";
+            $this->logs($act);
+            
+        }
+        
+        function verifyusername($user){
+            $q = "select * from userdata where username='$user'";
+            $r = mysql_query($q);
+            if(mysql_num_rows($r) < 1){
+               return true;
+            }else{
+                return false;   
+            }
+        }
+        
+        function getuser($search){
+            $user = $_SESSION['id'];
+            $q = "select * from userdata where username !='$user' and username like '%$search%' order by lname asc";   
+            $r = mysql_query($q);
+            return $r;
+        }
+    }
 
-include '../database.php';
-  
-function getMessages()   {
-    $userId = $_SESSION['user_id'];
-
-    $selects = 'messages.id, userdata.fname, userdata.lname, messages.content, messages.created_at, message_replies.sender_id';
-    $joins = 'LEFT JOIN messages ON userdata.id  = messages.sender_id LEFT JOIN message_replies ON messages.id = message_replies.message_id';
-    $orders = 'ORDER BY created_at DESC';
-
-    $query = "SELECT {$selects} FROM userdata {$joins} where messages.receiver_id = {$userId} {$orders}";
-    $rows = mysql_query($query);
-
-    return $rows; 
-}
-function getPosts()   {
-  $userId = $_SESSION['user_id'];
-
-  $selects = 'posts.id, userdata.fname, userdata.lname, posts.content, posts.created_at';
-  $joins = 'INNER JOIN posts ON userdata.id  = posts.author_id';
-  $orders = 'ORDER BY posts.created_at DESC';
-
-  $query = "SELECT {$selects} FROM userdata {$joins} where posts.author_id = {$userId} {$orders}";
-  $rows = mysql_query($query);
-
-  return $rows; 
-}
-
-if(isset($_POST['deleteMessage'])) {
-  // sql to delete a record
-  $sql = "DELETE FROM messages WHERE id = {$_POST['message_id']}";
-
-  // use exec() because no results are returned
-  $connection->exec($sql);
-  
-  echo "Record deleted successfully";
-}
-if(isset($_POST['deletePost'])) {
-  // sql to delete a record
-  $sql = "DELETE FROM posts WHERE id = {$_POST['post_id']}";
-
-  // use exec() because no results are returned
-  $connection->exec($sql);
-  
-  echo "Record deleted successfully";
-}
-
-if(isset($_POST['replyMessage'])) {
-  $messageId = $_POST['message_id'];
-  $userId = $_SESSION['user_id'];
-  $messageContent = $_POST['content'];
-
-  $sql = "INSERT INTO message_replies (message_id, sender_id, content)
-  VALUES (?, ?, ?)";
-
-  $connection->prepare($sql)->execute([$messageId, $userId, $messageContent]);
-  
-  echo "Record added successfully";
-
-  echo "<meta http-equiv='refresh' content='0'>";
-}
 ?>
