d202f3d9a1ac5622ef8e661d27c6eb71e063c6a6
Ryyan <132124759+Raiden-source@users.noreply.github.com>
2023-10-16 19:11:27 +0800

    Update users.php

--- admin/users.php
+++ admin/users.php
@@ -14,7 +14,40 @@
     $pdo_statement = $connection->prepare($sql);
     $pdo_statement->execute();
     $userdatas = $pdo_statement->fetchAll();
+
+    function deleteUser($userId, $connection) {
+        // Write your code to delete the subject with the given subject ID
+        $sql = "DELETE FROM userdata WHERE id = ?";
+        $stmt = $connection->prepare($sql);
+        $stmt->execute([$userId]);
+    }
+    
+        // Check if the delete button is clicked
+    if (isset($_POST['deleteUser']) && isset($_POST['userId'])) {
+        $userId = $_POST['userId'];
+        $delete = deleteUser($userId, $connection);
+    
+        if($delete){
+            // Redirect to the same page after the deletion
+            echo "<script type='text/javascript'>window.location.href = 'users.php?r=deleted';</script>";
+            exit(); // Make sure to exit after redirecting to prevent further code execution
+        }else{
+    
+        }
+    
+    }
 ?>
+<style>
+    /* Custom styling for the delete button */
+    .delete-button {
+    border: none;
+    background: none;
+    padding: 0;
+    color: #f00; /* Change the color as per your preference */
+    cursor: pointer;
+    outline: none; /* Remove outline on focus */
+    }
+</style>
 <div id="page-wrapper">
 
     <div class="container-fluid">
@@ -68,7 +101,15 @@
                                 <td class="text-center"><?php echo $userdata['username'];?></td>
                                 <td class="text-center"><?php echo $userdata['lname'].', '.$userdata['fname']?></td>
                                 <td class="text-center"><?php echo $userdata['level'];?></td>
-                                <td class="text-center"><a href="data/data_model.php?q=delete&table=userdata&id=<?php echo $userdata['id']?>" title="Remove" class="text-danger confirmation"><i class="fa fa-trash-o fa-lg text-danger confirmation"></i></a></a></td>
+                                <td class="text-center">
+                                <form class="delete-form" method="post">
+                                            <input type="hidden" name="userId" value="<?php echo $userdata['id']; ?>">
+                                            <button type="submit" name="deleteUser" class="delete-button" title="Remove" onclick="return confirm('Are you sure you want to delete this user?');">
+                                                <i class="fa fa-trash-o fa-lg text-danger"></i>
+                                            </button>
+                                        </form>
+                                    <!-- <a href="data/data_model.php?q=delete&table=userdata&id=<?php echo $userdata['id']?>" title="Remove" class="text-danger confirmation"><i class="fa fa-trash-o fa-lg text-danger confirmation"></i></a> -->
+                                </td>
                             </tr>
                         <?php endforeach; ?>
                         </tbody>
