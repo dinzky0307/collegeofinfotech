9f6b3e73e797c561299943b6b1365d94e9ff6053
Ryyan <132124759+Raiden-source@users.noreply.github.com>
2023-10-16 20:17:07 +0800

    Update get_subject_description.php

--- get_subject_description.php
+++ get_subject_description.php
@@ -1,1 +1,16 @@
+<?php
+
+if (isset($_POST['code'])) {
+    $code = $_POST['code'];
+    $stmt = $connection->prepare("SELECT title FROM subject WHERE code = ?");
+    $stmt->execute([$code]);
+    $result = $stmt->fetch(PDO::FETCH_ASSOC);
+
+    if ($result) {
+        echo $result['title'];
+    } else {
+        echo "";
+    }
+}
+?>
 
