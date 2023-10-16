b155cd0c1c4e83d933e77b59acb72ac99d3b1948
Ryyan <132124759+Raiden-source@users.noreply.github.com>
2023-10-16 17:50:25 +0800

    Update student_model.php

--- admin/data/student_model.php
+++ admin/data/student_model.php
@@ -1,13 +1,15 @@
 <?php
-include('../database.php');
-    $student = new Datastudent();
-    if(isset($_GET['q'])){
-        $student->$_GET['q']();
-    }
+    // include('../database.php'); // Assuming this file contains the database connection code
+    
     class Datastudent {
         
-        function __construct(){
-            if(!isset($_SESSION['id'])){
+        private $connection; // Add a private property to store the connection
+
+        // Accept $connection as a parameter in the constructor
+        function __construct($connection) {
+            $this->connection = $connection;
+            
+            if (!isset($_SESSION['id'])) {
                 header('location:../../');   
             }
         }
@@ -19,15 +21,97 @@ include('../database.php');
             mysql_query($q);
             return true;
         }
+        function getAvailableYears() {
+            $q = "SELECT DISTINCT year FROM student";
+            $result = mysql_query($q);
+            $years = array();
+    
+            while ($row = mysql_fetch_assoc($result)) {
+                $years[] = $row['year'];
+            }
+    
+            return $years;
+        }
+
+        function getAvailableSections() {
+            $q = "SELECT DISTINCT section FROM student";
+            $result = mysql_query($q);
+            $sections = array();
+    
+            while ($row = mysql_fetch_assoc($result)) {
+                $sections[] = $row['section'];
+            }
+    
+            return $sections;
+        }
+
+        function getstudent($search, $year = null, $section = null, $academic_year = null, $semester = null)
+        {
+            $query = "SELECT * FROM student";
+            $queryParams = array();
+            $hasConditions = false; // Keep track of whether any conditions have been added
+
+            // Add the WHERE clause only when searching or when academic year is specified
+            if (!empty($search)) {
+                $query .= " WHERE (studid LIKE :search OR fname LIKE :search OR lname LIKE :search)";
+                $queryParams[':search'] = '%' . $search . '%';
+                $hasConditions = true;
+            }
+
+            // Add the conditions for year, section, and academic_year if they are not empty
+            if (!empty($year)) {
+                $query .= $hasConditions ? " AND year = :year" : " WHERE year = :year";
+                $queryParams[':year'] = $year;
+                $hasConditions = true;
+            }
+
+            if (!empty($section)) {
+                $query .= $hasConditions ? " AND section = :section" : " WHERE section = :section";
+                $queryParams[':section'] = $section;
+                $hasConditions = true;
+            }
+
+            if (!empty($academic_year) && !empty($semester)) {
+                $query .= $hasConditions ? " AND ay = :academic_year AND semester = :semester" : " WHERE ay = :academic_year AND semester = :semester";
+                $queryParams[':academic_year'] = $academic_year;
+                $queryParams[':semester'] = $semester;
+            }
+
+            // if (!empty($semester)) {
+            //     $query .= $hasConditions ? " AND semester = :semester" : " WHERE semester = :semester";
+            //     $queryParams[':semester'] = $semester;
+            // }
+
+            $query .= " ORDER BY lname, fname, studid ASC";
+
+            $pdo_statement = $this->connection->prepare($query);
+
+            // Bind parameters only when they are present in the $queryParams array
+            foreach ($queryParams as $param => $value) {
+                $pdo_statement->bindValue($param, $value);
+            }
+
+            $pdo_statement->execute();
+            $students = $pdo_statement->fetchAll(PDO::FETCH_ASSOC);
+
+            return $students;
+        }
+
+        function getStudentInfoById($studid)
+        {
+            $query = "SELECT * FROM student WHERE studid = :studid";
+            $stmt = $this->connection->prepare($query);
+            $stmt->bindParam(':studid', $studid, PDO::PARAM_INT);
+            $stmt->execute();
         
-        //get all student info
-        function getstudent($search){
-            $q = "select * from student where studid like '%$search%' or fname like '%$search%' or lname like '%$search%' order by lname,fname,studid";
-            $r = mysql_query($q);
-            
-            return $r;
+            if ($stmt->rowCount() > 0) {
+                return $stmt->fetch(PDO::FETCH_ASSOC);
+            }
+        
+            return null;
         }
         
+
         //get class by ID
         function getstudentbyid($id){
             $q = "select * from student where id=$id";
@@ -129,3 +213,4 @@ $pdo_statement = $connection->prepare($query);
 $pdo_statement->bindValue(':keyword', '%' . '%', PDO::PARAM_STR);
 $pdo_statement->execute();
 $students = $pdo_statement->fetchAll();
+
