7cb37ba9e87904184833e3c5dd5314779b353901
Ryyan <132124759+Raiden-source@users.noreply.github.com>
2023-10-16 19:26:13 +0800

    Update grade.php

--- student/grade.php
+++ student/grade.php
@@ -1,120 +1,162 @@
 <?php
-    
-    $grade = new Datagrade();
 
-    function __construct(){
-            if(!isset($_SESSION['id'])){
-                header('location:../../');   
-            }
-        }
+$grade = new Datagrade();
 
-    class Datagrade {
-        
-        function __construct(){
-            if(!isset($_SESSION['id'])){
-                header('location:../../');   
-            }
-        }
-        
-        function getid(){
-            $studid = $_SESSION['id'];
-            $r = mysql_query("select * from student where studid='$studid'");
-            $row = mysql_fetch_array($r);
-            $id = $row['id'];
-            return $id;
-        }
+function __construct()
+{
+     if (!isset($_SESSION['id'])) {
+          header('location:../../');
+     }
+}
 
-        
-        function getsubject(){  
+class Datagrade
+{
+
+     function __construct()
+     {
+          if (!isset($_SESSION['id'])) {
+               header('location:../../');
+          }
+     }
+
+     function getid()
+     {
+          $studid = $_SESSION['id'];
+          $r = mysql_query("select * from student where studid='$studid'");
+          $row = mysql_fetch_array($r);
+          $id = $row['id'];
+          return $id;
+     }
+
+
+     function getsubject()
+     {
           $id = $this->getid();
           $q = "select * from studentsubject where studid=$id";
 
           if (isset($_GET['year_semester'])) {
                $year = $_GET['year'];
                $sem = $_GET['semester'];
-               $q.= " AND year = '$year' AND semester = '$sem'";
-           } else {
-               $q.= " AND year = '1' AND semester = 'First Semester'";
-           }
+               $q .= " AND year = '$year' AND semester = '$sem'";
+          } else {
+               $q .= " AND year = '1' AND semester = 'First Semester'";
+          }
 
-            $r = mysql_query($q);
-            $data = array();
-            while($row = mysql_fetch_array($r)){
-                $classid = $row['classid'];
-                $q2 = "select * from class where id=$classid";
-                $r2 = mysql_query($q2);  
-                $data[] = mysql_fetch_array($r2);
-            }
-            return $data;
-        }
-        
-        function getsubjectitle($code){
-            $q = "select * from subject where code='$code'";
-            $r = mysql_query($q);
-            $data = array();
-            $data[] = mysql_fetch_array($r);
-            return $data;
-        }
+          $r = mysql_query($q);
+          $data = array();
+          while ($row = mysql_fetch_array($r)) {
+               $classid = $row['classid'];
+               $year = $row['year'];
+               $section = $row['section'];
+               $sem = $row['semester'];
+               $SY = $row['SY'];
+               $subjectid = $row['subjectid'];
 
-        function getsemester(){  
+               $q3 = "select * from subject where id=$subjectid";
+               $r3 = mysql_query($q3);
+               while ($srow = mysql_fetch_array($r3)) {
+                    $subjectcode = $srow['code'];
+
+                    $q2 = "select * from class where year=$year AND section='$section' AND sem='$sem' AND SY='$SY' AND subject='$subjectcode'";
+                    $r2 = mysql_query($q2);
+                    $data[] = mysql_fetch_array($r2);
+               }
+          }
+          return $data;
+     }
+
+     function getsubjectitle($code)
+     {
+          $q = "select * from subject where code='$code'";
+          $r = mysql_query($q);
+          $data = array();
+          $data[] = mysql_fetch_array($r);
+          return $data;
+     }
+
+     function getsemester()
+     {
 
           $id = $this->getid();
           $q = "select * from student where studid=$id";
           $r = mysql_query($q);
           $data = array();
-          while($row = mysql_fetch_array($r)){
-              $classid = $row['classid'];
-              $q2 = "select * from class where id=$classid";
-              $r2 = mysql_query($q2);  
-              $data[] = mysql_fetch_array($r2);
+          while ($row = mysql_fetch_array($r)) {
+               $classid = $row['classid'];
+               $q2 = "select * from class where id=$classid";
+               $r2 = mysql_query($q2);
+               $data[] = mysql_fetch_array($r2);
           }
           return $data;
-      }
-        
-        function getgrade($classid){
-            $studid = $this->getid();
-            $q = "select * from studentsubject where studid='$studid' and classid='$classid'";
-            $r = mysql_query($q);
-            if($row = mysql_fetch_array($r)){
+     }
 
-            //    $prelim_grade = ($row['prelim_grade']);
-               $midterm_grade = ($row['midterm_grade']);
-               $finals_grade = ($row['final_grade']);
-               
-            //    $prelim = $prelim_grade;
-               $midterm = $midterm_grade;
-               $final = $finals_grade; 
-                
-               $total = ($midterm * .30) + ($final * .70);
-                
-                $data = array(
-                    // 'eqprelim' => $this->gradeconversion($prelim),
-                    'eqmidterm' => $this->gradeconversion($midterm),
-                    'eqfinal' => $this->gradeconversion($final),
-                    'eqtotal' => $this->gradeconversion($total),
-                    // 'prelim' => round($prelim),
-                    'midterm' => round($midterm),
-                    'final' => round($final),
-                    'total' => round($total),
-                    // 'prelim_grade' => $row['prelim_grade'],
-                    'midterm_grade' => $row['midterm_grade'],
-                    'finals_grade' => $row['final_grade'],
-                );
-            }
-            
-            return $data;
+     function getgrade($year, $section, $sem, $sy, $subject)
+     {
+          $studid = $this->getid();
+          $data = array();
+
+          $q3 = "SELECT * FROM subject WHERE code='$subject'";
+          $r3 = mysql_query($q3);
+
+          if ($r3) {
+               $subjectcode = '';
+               while ($srow = mysql_fetch_array($r3)) {
+                    $subjectcode = $srow['id'];
+               }
+
+               $q = "SELECT * FROM studentsubject WHERE studid='$studid' AND year='$year' AND section='$section' AND semester='$sem' AND SY='$sy' AND subjectid='$subjectcode'";
+               $r = mysql_query($q);
+
+               if ($r) {
+                    if ($row = mysql_fetch_array($r)) {
+                         $prelim_grade = ($row['prelim_grade']);
+                         $midterm_grade = ($row['midterm_grade']);
+                         $finals_grade = ($row['final_grade']);
+
+                         $prelim = $prelim_grade;
+                         $midterm = $midterm_grade;
+                         $final = $finals_grade;
+
+                         $total = ($prelim * .30) + ($midterm * .30) + ($final * .40);
+
+                         $data = array(
+                              'eqprelim' => $this->gradeconversion($prelim),
+                              'eqmidterm' => $this->gradeconversion($midterm),
+                              'eqfinal' => $this->gradeconversion($final),
+                              'eqtotal' => $this->gradeconversion($total),
+                              'prelim' => round($prelim),
+                              'midterm' => round($midterm),
+                              'final' => round($final),
+                              'total' => round($total),
+                              'prelim_grade' => $row['prelim_grade'],
+                              'midterm_grade' => $row['midterm_grade'],
+                              'finals_grade' => $row['final_grade'],
+                         );
+                    }
+               } else {
+                    // Handle query execution error
+                    echo "Query execution failed 1: " . mysql_error();
+               }
+          } else {
+               // Handle query execution error
+               echo "Query execution failed 2: " . mysql_error();
           }
-          
-        function gradeconversion($grade){
-            $grade = round($grade);
-            if($grade==0){
-                 $data = 0;
-               }else{
-                switch ($grade) {
-                     case $grade > 94:
+
+          return $data;
+     }
+
+
+     function gradeconversion($grade)
+     {
+          $grade = round($grade);
+          if ($grade == 0) {
+               $data = 0;
+          } else {
+               switch ($grade) {
+                    case $grade > 94:
                          $data = 1.0;
                          break;
-                     case 94:
+                    case 94:
                          $data = 1.1;
                          break;
                     case 93:
@@ -159,7 +201,7 @@
                     case 80:
                          $data = 2.5;
                          break;
-                   case 79:
+                    case 79:
                          $data = 2.6;
                          break;
                     case 78:
@@ -203,23 +245,24 @@
                          break;
                     case 65:
                          $data = 4.0;
-                         break; 
+                         break;
 
 
 
 
-                     default:
+                    default:
                          $data = 5.0;
-                }
-            }
-           return sprintf("%.1f",$data);
-        }
-        function getteacher($teachid){
-            $r = mysql_query("select * from teacher where id=$teachid");
-            $result = mysql_fetch_array($r);
-            $data = $result['fname'].' '.$result['lname'];
-            return $data;
-        }
-        
-    }
+               }
+          }
+          return sprintf("%.1f", $data);
+     }
+     function getteacher($teachid)
+     {
+          $r = mysql_query("select * from teacher where id=$teachid");
+          $result = mysql_fetch_array($r);
+          $data = $result['fname'] . ' ' . $result['lname'];
+          return $data;
+     }
+
+}
 ?>
