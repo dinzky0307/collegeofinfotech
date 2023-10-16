2f42055119aae622707fce70368a8cf9dac7c227
Ryyan <132124759+Raiden-source@users.noreply.github.com>
2023-10-16 19:16:38 +0800

    Update connection.php

--- student/connection.php
+++ student/connection.php
@@ -1,1 +1,12 @@
+<?php
 
+define('DB_SERVER', '127.0.0.1');
+define('DB_USERNAME', 'infotechmcc');
+define('DB_PASSWORD', 'infotechmcc');
+define('DB_NAME', 'infotechmcc');
+$dbconnection = mysqli_connect(DB_SERVER, DB_USERNAME, DB_PASSWORD, DB_NAME);
+if($dbconnection === false){
+    die("ERROR: Could not connect. " . mysqli_connect_error());
+}
+
+?>
