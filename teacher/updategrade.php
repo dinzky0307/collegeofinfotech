78b58720412da925d661b426d9fadc8f70bb580f
Ryyan <132124759+Raiden-source@users.noreply.github.com>
2023-11-18 20:38:03 +0800

    Update updategrade.php

--- teacher/updategrade.php
+++ teacher/updategrade.php
@@ -2,19 +2,18 @@
 
 include 'connection.php';
 
-if(isset($_GET['updategrade'])){
   
-  $id = $_GET['id'];
-  $prelim = $_GET['p'];
-  $midterm = $_GET['m'];
-  $final = $_GET['f'];
-  $classid = $_GET['c'];
-  $year = $_GET['y'];
-  $sem = $_GET['s'];
-  $sec = $_GET['e'];
-  $ay = $_GET['a'];
-  $sub = $_GET['subject'];
-  $code = $_GET['cd'];
+  $id = $_POST['id'];
+  $prelim = $_POST['p'];
+  $midterm = $_POST['m'];
+  $final = $_POST['f'];
+  $classid = $_POST['c'];
+  $year = $_POST['y'];
+  $sem = $_POST['s'];
+  $sec = $_POST['e'];
+  $ay = $_POST['a'];
+  $sub = $_POST['subject'];
+  $code = $_POST['cd'];
   $total = ($prelim + $midterm + $final) / 3;
   
   
@@ -22,12 +21,11 @@ if(isset($_GET['updategrade'])){
     $sql = "UPDATE studentsubject SET prelim_grade='$prelim', midterm_grade='$midterm', final_grade='$final', total='$total' WHERE studid='$id' AND subjectid = '{$sub}' AND year='$year' AND semester='$sem' AND section='$sec' AND SY='$ay'";
   
     if ($dbconnection->query($sql) === TRUE) {
-      echo "<script>window.location.href='student.php?classid=".$classid."&sem=".$sem."&sec=".$sec."&ay=".$ay."&code=".$code."&y=".$year." '</script>";
+      // echo "<script>window.location.href='student.php?classid=".$classid."&sem=".$sem."&sec=".$sec."&ay=".$ay."&code=".$code."&y=".$year." '</script>";
     } else {
       echo "Error updating record: " . $dbconnection->error;
     }
   
-}
 
 
 ?>
