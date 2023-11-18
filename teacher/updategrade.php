63956e0894e94bc38314651768073d08ad5e9373
Ryyan <132124759+Raiden-source@users.noreply.github.com>
2023-11-18 19:09:18 +0800

    Update updategrade.php

--- teacher/updategrade.php
+++ teacher/updategrade.php
@@ -11,15 +11,15 @@ $year = $_GET['y'];
 $sem = $_GET['s'];
 $sec = $_GET['e'];
 $ay = $_GET['a'];
-$sub = $_GET['b'];
+$sub = $_GET['sub'];
 $code = $_GET['cd'];
 $total = ($prelim + $midterm + $final) / 3;
 
 
-  $sql = "UPDATE studentsubject SET prelim_grade='$prelim', midterm_grade='$midterm', final_grade='$final', total='$total' WHERE studid='$id' AND year='$year' AND classid = $classid AND semester='$sem' AND section='$sec' AND SY='$ay'";
+  $sql = "UPDATE studentsubject SET prelim_grade='$prelim', midterm_grade='$midterm', final_grade='$final', total='$total' WHERE studid='$id' AND year='$year' subjectid = '$sub' AND semester='$sem' AND section='$sec' AND SY='$ay'";
 
   if ($dbconnection->query($sql) === TRUE) {
-    echo "<script>window.location.href='student.php?classid=".$classid."&sem=".$sem."&sec=".$sec."&ay=".$ay."&code=".$code."&y=".$year." '</script>";
+    echo "<script>window.location.href='student.php?classid=".$classid."&sem=".$sem."&sec=".$sec."&ay=".$ay."&code=".$code."&sub=".$sub."&y=".$year." '</script>";
   } else {
     echo "Error updating record: " . $dbconnection->error;
   }
