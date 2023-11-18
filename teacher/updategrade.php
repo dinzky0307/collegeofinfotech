680ab1325b428910fd981265f4153de884c309ae
Ryyan <132124759+Raiden-source@users.noreply.github.com>
2023-11-18 19:51:11 +0800

    Update updategrade.php

--- teacher/updategrade.php
+++ teacher/updategrade.php
@@ -2,33 +2,31 @@
 
 include 'connection.php';
 
-$id = $_GET['id'];
-$prelim = $_GET['p'];
-$midterm = $_GET['m'];
-$final = $_GET['f'];
-$classid = $_GET['c'];
-$year = $_GET['y'];
-$sem = $_GET['s'];
-$sec = $_GET['e'];
-$ay = $_GET['a'];
-$sub = $_GET['subject'];
-$code = $_GET['cd'];
-$total = ($prelim + $midterm + $final) / 3;
-
-
-  $sql = "UPDATE studentsubject SET prelim_grade='$prelim', midterm_grade='$midterm', final_grade='$final', total='$total' WHERE studid='$id' AND subjectid = '$sub' AND year='$year' AND semester='$sem' AND section='$sec' AND SY='$ay'";
-
-  if ($dbconnection->query($sql) === TRUE) {
-    echo "<script>window.location.href='student.php?classid=".$classid."&sem=".$sem."&sec=".$sec."&ay=".$ay."&code=".$code."&y=".$year." '</script>";
-  } else {
-    echo "Error updating record: " . $dbconnection->error;
-  }
-
-exit();
-
-
-
-
+if(isset($_GET['updategrade'])){
+  $id = $_GET['id'];
+  $prelim = $_GET['p'];
+  $midterm = $_GET['m'];
+  $final = $_GET['f'];
+  $classid = $_GET['c'];
+  $year = $_GET['y'];
+  $sem = $_GET['s'];
+  $sec = $_GET['e'];
+  $ay = $_GET['a'];
+  $sub = $_GET['subject'];
+  $code = $_GET['cd'];
+  $total = ($prelim + $midterm + $final) / 3;
+  
+  
+    $sql = "UPDATE studentsubject SET prelim_grade='$prelim', midterm_grade='$midterm', final_grade='$final', total='$total' WHERE studid='$id' AND subjectid = '$sub' AND year='$year' AND semester='$sem' AND section='$sec' AND SY='$ay'";
+  
+    if ($dbconnection->query($sql) === TRUE) {
+      echo "<script>window.location.href='student.php?classid=".$classid."&sem=".$sem."&sec=".$sec."&ay=".$ay."&code=".$code."&y=".$year." '</script>";
+    } else {
+      echo "Error updating record: " . $dbconnection->error;
+    }
+  
+  exit();
+}
 
 
 ?>
