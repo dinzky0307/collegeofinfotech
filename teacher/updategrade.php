421ad003d03465d470ad6365dd74126da3160613
Ryyan <132124759+Raiden-source@users.noreply.github.com>
2023-11-18 19:54:47 +0800

    Update updategrade.php

--- teacher/updategrade.php
+++ teacher/updategrade.php
@@ -19,11 +19,11 @@ if(isset($_GET['updategrade'])){
   
     $sql = "UPDATE studentsubject SET prelim_grade='$prelim', midterm_grade='$midterm', final_grade='$final', total='$total' WHERE studid='$id' AND subjectid = '$sub' AND year='$year' AND semester='$sem' AND section='$sec' AND SY='$ay'";
   
-    if ($dbconnection->query($sql)) {
-      echo "<script>window.location.href='student.php?classid=".$classid."&sem=".$sem."&sec=".$sec."&ay=".$ay."&code=".$code."&y=".$year." '</script>";
-    } else {
-      echo "Error updating record: " . $dbconnection->error;
-    }
+    // if ($dbconnection->query($sql)) {
+    //   echo "<script>window.location.href='student.php?classid=".$classid."&sem=".$sem."&sec=".$sec."&ay=".$ay."&code=".$code."&y=".$year." '</script>";
+    // } else {
+    //   echo "Error updating record: " . $dbconnection->error;
+    // }
   
   exit();
 }
