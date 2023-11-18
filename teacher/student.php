b2d0a8f6c406af18987d8431b33b4bdf71e0007d
Ryyan <132124759+Raiden-source@users.noreply.github.com>
2023-11-18 21:10:58 +0800

    Update student.php

--- teacher/student.php
+++ teacher/student.php
@@ -174,18 +174,19 @@ $('.updategrade').click(function(){
     var midterm = $('#'+dataid+' #midterm').val();
     var final = $('#'+dataid+' #final').val();
     var classid = $(this).attr('data-gradeid');
-   
-    $.ajax({
-        type:"post",
-        url: "updategrade.php",
-        data: {id: dataid, p: prelim, m: midterm, f: final, c: classid},
-        success:function(){
-            window.location.href = "<?php echo $_SERVER['REQUEST_URI'] ?>"
-        }
-    })
+
+    console.log(classid)
+    // $.ajax({
+    //     type:"post",
+    //     url: "updategrade.php",
+    //     data: {id: dataid, p: prelim, m: midterm, f: final, c: classid},
+    //     success:function(){
+    //         window.location.href = "<?php echo $_SERVER['REQUEST_URI'] ?>"
+    //     }
+    // })
 
     // $('#'+dataid+' .updategrade').attr('href','updategrade.php?updategrade&id='+dataid+'&p='+prelim+'&m='+midterm+'&f='+final+'&c='+classid+'&y='+year+'&s='+sem+'&e='+sec+'&a='+ay+'&subject='+subject+'&cd='+code);
-    $('.loading').show();
+    // $('.loading').show();
 
 });
 </script>
