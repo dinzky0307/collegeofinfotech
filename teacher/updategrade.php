2bada01f45aa0dfc00e7e3c41d59ca7063030ad0
Ryyan <132124759+Raiden-source@users.noreply.github.com>
2023-11-18 18:26:52 +0800

    Update updategrade.php

--- teacher/updategrade.php
+++ teacher/updategrade.php
@@ -15,14 +15,28 @@ $sub = $_GET['b'];
 $code = $_GET['cd'];
 $total = ($prelim + $midterm + $final) / 3;
 
-$sql = "UPDATE studentsubject SET prelim_grade='$prelim', midterm_grade='$midterm', final_grade='$final', total='$total' WHERE studid='$id' AND year='$year' AND subjectid = '$sub' AND semester='$sem' AND section='$sec' AND SY='$ay'";
+
+$get_subject = $dbconnection->query("SELECT * FROM subject WHERE code = '$code' ");
+
+if($get_subject->num_rows > 0){
+  $subject = $get_subject->fetch_assoc();
+
+  $sub_id = $subject['id'];
 
 
-if ($dbconnection->query($sql) === TRUE) {
-	echo "<script>window.location.href='student.php?classid=".$classid."&sem=".$sem."&sec=".$sec."&ay=".$ay."&code=".$code."&y=".$year." '</script>";
-} else {
-  echo "Error updating record: " . $dbconnection->error;
+  $sql = "UPDATE studentsubject SET prelim_grade='$prelim', midterm_grade='$midterm', final_grade='$final', total='$total' WHERE studid='$id' AND year='$year' AND subjectid = '$sub_id' AND semester='$sem' AND section='$sec' AND SY='$ay'";
+
+  if ($dbconnection->query($sql) === TRUE) {
+    echo "<script>window.location.href='student.php?classid=".$classid."&sem=".$sem."&sec=".$sec."&ay=".$ay."&code=".$code."&y=".$year." '</script>";
+  } else {
+    echo "Error updating record: " . $dbconnection->error;
+  }
+
 }
+
+
+
+
 
 
 ?>
