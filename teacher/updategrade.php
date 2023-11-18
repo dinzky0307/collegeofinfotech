5595ec9935dbd3498bc778e0aa18c2df90cc80e1
Ryyan <132124759+Raiden-source@users.noreply.github.com>
2023-11-18 19:13:35 +0800

    Update updategrade.php

--- teacher/updategrade.php
+++ teacher/updategrade.php
@@ -16,7 +16,7 @@ $code = $_GET['cd'];
 $total = ($prelim + $midterm + $final) / 3;
 
 
-  $sql = "UPDATE studentsubject SET prelim_grade='$prelim', midterm_grade='$midterm', final_grade='$final', total='$total' WHERE studid='$id' AND year='$year' AND subjectid = '$sub'  semester='$sem' AND section='$sec' AND SY='$ay'";
+  $sql = "UPDATE studentsubject SET prelim_grade='$prelim', midterm_grade='$midterm', final_grade='$final', total='$total' WHERE studid='$id' AND year='$year' AND subjectid = '$sub' AND semester='$sem' AND section='$sec' AND SY='$ay'";
 
   if ($dbconnection->query($sql) === TRUE) {
     echo "<script>window.location.href='student.php?classid=".$classid."&sem=".$sem."&sec=".$sec."&ay=".$ay."&code=".$code."&sub=".$sub."&y=".$year." '</script>";
