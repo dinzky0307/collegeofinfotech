9df2d3b549df898cbed1e8ba1ea316f81008826e
Ryyan <132124759+Raiden-source@users.noreply.github.com>
2023-11-21 16:39:17 +0800

    Update deletestudent.php

--- admin/data/deletestudent.php
+++ admin/data/deletestudent.php
@@ -1,7 +1,7 @@
 <?php
-// require('../../database.php');
-// $id=$_REQUEST['id'];
-// $query = "DELETE FROM student WHERE id=$id"; 
-// $result = mysqli_query($con,$query) or die ( mysqli_error());
-// header("Location: studentlist.php"); 
+require('../../database.php');
+$id=$_REQUEST['id'];
+$query = "DELETE FROM student WHERE id=$id"; 
+$result = mysqli_query($con,$query) or die ( mysqli_error());
+header("Location: studentlist.php"); 
 ?>
