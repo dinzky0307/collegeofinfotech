7e067a2ca2882b79c510a3034704721f2e335b85
Ryyan <132124759+Raiden-source@users.noreply.github.com>
2023-11-22 08:25:10 +0800

    Update ajax.php

--- admin/ajax.php
+++ admin/ajax.php
@@ -8,74 +8,74 @@ if($action == 'login'){
 	$login = $crud->login();
 	if($login)
 		echo $login;
-}
+}else
 if($action == 'login_faculty'){
 	$login_faculty = $crud->login_faculty();
 	if($login_faculty)
 		echo $login_faculty;
-}
+}else
 if($action == 'login2'){
 	$login = $crud->login2();
 	if($login)
 		echo $login;
-}
+}else
 if($action == 'logout'){
 	$logout = $crud->logout();
 	if($logout)
 		echo $logout;
-}
+}else
 if($action == 'logout2'){
 	$logout = $crud->logout2();
 	if($logout)
 		echo $logout;
-}
+}else
 if($action == 'save_user'){
 	$save = $crud->save_user();
 	if($save)
 		echo $save;
-}
+}else
 if($action == 'delete_user'){
 	$save = $crud->delete_user();
 	if($save)
 		echo $save;
-}
+}else
 if($action == 'signup'){
 	$save = $crud->signup();
 	if($save)
 		echo $save;
-}
+}else
 if($action == 'update_account'){
 	$save = $crud->update_account();
 	if($save)
 		echo $save;
-}
+}else
 if($action == "save_settings"){
 	$save = $crud->save_settings();
 	if($save)
 		echo $save;
-}
+}else
 if($action == "save_course"){
 	$save = $crud->save_course();
 	if($save)
 		echo $save;
-}
+}else
 
 if($action == "delete_course"){
 	$delete = $crud->delete_course();
 	if($delete)
 		echo $delete;
-}
+}else
 if($action == "save_subject"){
 	$save = $crud->save_subject();
 	if($save)
 		echo $save;
-}
+}else
 
 if($action == "delete_subject"){
 	$delete = $crud->delete_subject();
 	if($delete)
 		echo $delete;
-}
+}else
 if($action == "save_faculty"){
 	$save = $crud->save_faculty();
 	if($save)
@@ -85,91 +85,91 @@ if($action == "delete_faculty"){
 	$save = $crud->delete_faculty();
 	if($save)
 		echo $save;
-}
+}else
 
 if($action == "save_schedule"){
 	$save = $crud->save_schedule();
 	if($save)
 		echo $save;
-}
+}else
 if($action == "delete_schedule"){
 	$save = $crud->delete_schedule();
 	if($save)
 		echo $save;
-}
+}else
 if($action == "get_schecdule"){
 	$get = $crud->get_schecdule();
 	if($get)
 		echo $get;
-}
+}else
 if($action == "delete_forum"){
 	$save = $crud->delete_forum();
 	if($save)
 		echo $save;
-}
+}else
 
 if($action == "save_comment"){
 	$save = $crud->save_comment();
 	if($save)
 		echo $save;
-}
+}else
 if($action == "delete_comment"){
 	$save = $crud->delete_comment();
 	if($save)
 		echo $save;
 
-}
+}else
 
 if($action == "save_event"){
 	$save = $crud->save_event();
 	if($save)
 		echo $save;
-}
+}else
 if($action == "delete_event"){
 	$save = $crud->delete_event();
 	if($save)
 		echo $save;
-}	
+}else
 if($action == "participate"){
 	$save = $crud->participate();
 	if($save)
 		echo $save;
-}
+}else
 if($action == "get_venue_report"){
 	$get = $crud->get_venue_report();
 	if($get)
 		echo $get;
-}
+}else
 if($action == "save_art_fs"){
 	$save = $crud->save_art_fs();
 	if($save)
 		echo $save;
-}
+}else
 if($action == "delete_art_fs"){
 	$save = $crud->delete_art_fs();
 	if($save)
 		echo $save;
-}
+}else
 if($action == "get_pdetails"){
 	$get = $crud->get_pdetails();
 	if($get)
 		echo $get;
-}
+}else
 if($action == "updatesubject"){
 	$get = $crud->updatesubject($id);
 	// if($get)
 	// 	echo $get;
-}
+}else
 if($action == "updateclass"){
 	$get = $crud->updateclass($id);
 	// if($get)
 	// 	echo $get;
-}
+}else
 if($action == "updatestudent"){
 	$get = $crud->updatestudent($id);
 	// if($get)
 	// 	echo $get;
-}
+}else
 if($action == "updateteacher"){
 	$get = $crud->updateteacher($id);
 	// if($get)
