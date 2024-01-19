7ee9650f6b527290160ba175004952af5f2b2efd
Ryyan <132124759+Raiden-source@users.noreply.github.com>
2024-01-19 17:43:06 +0800

    Update updategrade.php

--- teacher/updategrade.php
+++ teacher/updategrade.php
@@ -15,7 +15,7 @@ include 'connection.php';
   $sub = $_POST['subject'];
   $code = $_POST['cd'];
   $grade_id = $_POST['grade_id'];
-  $total = (($midterm*0.3) + ($final*0.7)) ;
+  $total = ((($prelim+$midterm)/2)*0.3) + ($final*0.7)) ;
   
   
   
