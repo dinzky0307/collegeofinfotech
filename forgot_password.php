d32253455df15bdc7bda52c11bfe4d04685e4f9b
Ryyan <132124759+Raiden-source@users.noreply.github.com>
2024-01-22 13:28:21 +0800

    Update forgot_password.php

--- forgot_password.php
+++ forgot_password.php
@@ -1,20 +1,81 @@
 <?php
 include 'config.php';
+use PHPMailer\PHPMailer\PHPMailer;
+use PHPMailer\PHPMailer\Exception;
+use PHPMailer\PHPMailer\SMTP;
+
+require_once("phpmailer/src/Exception.php");
+require_once("phpmailer/src/PHPMailer.php");
+require_once("phpmailer/src/SMTP.php");
 
 if (isset($_POST['submit'])) {
-    $user = $_POST['user'];
+    $user = $_POST['email'];
     
     // Check if the provided ID number exists in the userdata table
-    $query = "SELECT * FROM userdata WHERE username='$user'";
+    $query = "SELECT * FROM teacher WHERE email = '$user' ";
+    $student = "SELECT * FROM student WHERE email = '$user' ";
     $result = mysql_query($query);
+
+    $result_student = mysql_query($student);
     
     if ($result && mysql_num_rows($result) == 1) {
-        header('location: reset-pass.php');
-        $_SESSION['username'] = $user;
-    } else {
+        $data = mysql_fetch_assoc($result);
+        $verification = uniqid(rand(2,5));
+        $id = $data['teachid'];
+
+      }else if($result_student && mysql_num_rows($result_student) == 1){
+        $data = mysql_fetch_assoc($result_student);
+        $verification = uniqid(rand(2,5));
+        $id = $data['studid'];
+      }
+       else {
         // User does not have an account, display an error message
-        $errorMessage = "No account found with the provided ID number.";
+        $errorMessage = "No account found with the provided Email account.";
     }
+        $email = $_POST['email'];
+
+        $mail = new PHPMailer(true);
+
+        $mail->SMTPDebug = 0; // Enable verbose debug output
+        $mail->isSMTP(); // Send using SMTP
+        $mail->Host = 'smtp.gmail.com'; // Set the SMTP server to send through
+        $mail->SMTPAuth = true; // Enable SMTP authentication
+        $mail->Username = 'enelyntribunalo@gmail.com'; // SMTP username
+        $mail->Password = 'tcuuujxrlvbxvdxn'; // SMTP password
+        $mail->SMTPSecure = 'tls'; // Enable TLS encryption; `PHPMailer::ENCRYPTION_SMTPS` also accepted
+        $mail->Port = 587; // TCP port to connect to
+
+        $mail->SMTPOptions = array(
+            'ssl' => array(
+                'verify_peer' => false,
+                'verify_peer_name' => false,
+                'allow_self_signed' => true
+            )
+        );
+
+        $mail->setFrom('enelyntribunalo@gmail.com', 'MCC Info Tech');
+        $mail->addAddress($email);
+        
+    
+        $session = $_SESSION['reset'] = $verification; 
+    
+        //Content
+        $mail->isHTML(true);                                  //Set email format to HTML
+        $mail->Subject = 'Reset Password';
+        $mail->Body    = "Click the link to reset password : <a href='https://infotechmcc.com/reset_pass.php?reset=$id&id=$session'>Click here</a>";
+    
+
+        $mail->send();
+
+        ?>
+        <script>
+            alert("Please check your email account for confirmation")
+            window.location.href = "forgot_password.php"
+        </script>
+        <?php 
+
+  
+    
 }
 ?>
 <style>
@@ -32,7 +93,7 @@ if (isset($_POST['submit'])) {
     <meta name="author" content="">
     <link rel="icon" href="img/mcc.png">
     <title>InfoTech</title>
-    <link rel="stylesheet" href="style.css">
+    <link rel="stylesheet" href="css/style1.css">
 </head>
 <body>
     <div class="container">
@@ -47,7 +108,7 @@ if (isset($_POST['submit'])) {
                     </div>
                     <div class="input-field">
                         <i class="fas fa-user"></i>
-                        <input type="text" placeholder="ID Number" name="user"  required />
+                        <input type="text" placeholder="Email" name="email"  required />
                     </div>
                     <input type="submit" value="Submit" name="submit" class="btn solid" />
                     <button type="button" class="btn cancel" id="cancelButton" style="height: 44px;">Cancel</button>
@@ -61,9 +122,9 @@ if (isset($_POST['submit'])) {
         <div class="content">
         <img src="img/mcc.png" alt="mcc" width="230" height="200" style="padding: 0 4px; margin-bottom: 95px; margin-right: 50px; margin-top: 30px;">
         <p class="p" style=" text-shadow: 1px 2px 3px black;
-  color: #f0f0f0;">Madridejos Community College</p>
+  color: #f0f0f0;"></p>
           <h2 class="h2" style=" text-shadow: 1px 3px 3px black;
-  color: #f0f0f0;">College of Computer</h2>
+  color: #f0f0f0;">College of Computer Studies</h2>
         </div>
         <!-- <img src="img/register.svg" class="image" alt=""/> -->
       </div>
