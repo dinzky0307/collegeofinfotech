e653cec5be12b9492c39eb9e605898a808cbf138
Ryyan <132124759+Raiden-source@users.noreply.github.com>
2024-01-23 12:53:51 +0800

    Update studentsubject.php

--- admin/studentsubject.php
+++ admin/studentsubject.php
@@ -209,7 +209,9 @@ function gradeconversion($grade){
                 <?php
                 if (isset($grade['prelim_grade']) && isset($grade['midterm_grade']) && isset($grade['final_grade'])) {
                     $finalRatings = $student->gradeconversion(array_sum([
-                        (($grade['prelim_grade'] + $grade['midterm_grade'])/2) * 0.3, $grade['final_grade'] * 0.7
+                        $grade['prelim_grade'] * 0.3,
+                        $grade['midterm_grade'] * 0.3,
+                        $grade['final_grade'] * 0.7
                     ]));
                     echo $finalRatings;
                 } else {
@@ -221,7 +223,9 @@ function gradeconversion($grade){
                 <?php
                 if (isset($grade['prelim_grade']) && isset($grade['midterm_grade']) && isset($grade['final_grade'])) {
                     $average = $student->gradeconversion(array_sum([
-                            (($grade['prelim_grade'] + $grade['midterm_grade'])/2) * 0.3, $grade['final_grade'] * 0.7
+                        $grade['prelim_grade'] * 0.3,
+                        $grade['midterm_grade'] * 0.3,
+                        $grade['final_grade'] * 0.7
                     ]));
                     echo getRemarks($average);
                 } else {
