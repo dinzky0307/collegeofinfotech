68586f6a60bedf7770fe04ea6bc5939dcb646a91
Ryyan <132124759+Raiden-source@users.noreply.github.com>
2024-02-05 23:45:00 +0800

    Update grade.php

--- student/grade.php
+++ student/grade.php
@@ -140,112 +140,49 @@ function getgrade($year, $section, $sem, $sy, $subject)
 
 
 
-      function gradeconversion($grade){
-            $grade = round($grade);
-            if($grade==0){
-                 $data = 0;
-            }else{
-                switch ($grade) {
-                     case 1.0:
-                         $data = 1.0;
-                         break;
-                     case 1.1:
-                         $data = 1.1;
-                         break;
-                    case 1.2:
-                         $data = 1.2;
-                         break;
-                    case 1.3:
-                         $data = 1.3;
-                         break;
-                    case 1.4:
-                         $data = 1.4;
-                         break;
-                    case 1.5:
-                         $data = 1.5;
-                         break;
-                    case 1.6:
-                         $data = 1.6;
-                         break;
-                    case 1.7:
-                         $data = 1.7;
-                         break;
-                    case 1.8:
-                         $data = 1.8;
-                         break;
-                    case 1.9:
-                         $data = 1.9;
-                         break;
-                    case 2.0:
-                         $data = 2.0;
-                         break;
-                    case 2.1:
-                         $data = 2.1;
-                         break;
-                    case 2.2:
-                         $data = 2.2;
-                         break;
-                    case 2.3:
-                         $data = 2.3;
-                         break;
-                    case 2.4:
-                         $data = 2.4;
-                         break;
-                    case 2.5:
-                         $data = 2.5;
-                         break;
-                   case 2.6:
-                         $data = 2.6;
-                         break;
-                    case 2.7:
-                         $data = 2.7;
-                         break;
-                    case 2.8:
-                         $data = 2.8;
-                         break;
-                    case 2.9:
-                         $data = 2.9;
-                         break;
-                    case 3.0:
-                         $data = 3.0;
-                         break; 
-                      case 3.1:
-                         $data = 3.1;
-                         break;
-                    case 3.2:
-                         $data = 3.2;
-                         break;
-                    case 3.3:
-                         $data = 3.3;
-                         break;
-                    case 3.4:
-                         $data = 3.4;
-                         break; 
-                    case 3.5:
-                         $data = 3.5;
-                         break;
-                    case 3.6:
-                         $data = 3.6;
-                         break;
-                    case 3.7:
-                         $data = 3.7;
-                         break;
-                    case 3.8:
-                         $data = 3.8;
-                         break;
-                   case 3.9:
-                         $data = 3.9;
-                         break;
-                   case 4.0:
-                         $data = 4.0;
-                         break;
+function gradeconversion($grade)
+{
+    $data = 5.0;  // Default value
 
-                     default:
-                         $data = 5.0;
-                }
-            }
-            return sprintf("%.1f",$data);
-        }
+    switch ($grade) {
+        case 1.0:
+        case 1.1:
+        case 1.2:
+        case 1.3:
+        case 1.4:
+        case 1.5:
+        case 1.6:
+        case 1.7:
+        case 1.8:
+        case 1.9:
+        case 2.0:
+        case 2.1:
+        case 2.2:
+        case 2.3:
+        case 2.4:
+        case 2.5:
+        case 2.6:
+        case 2.7:
+        case 2.8:
+        case 2.9:
+        case 3.0:
+        case 3.1:
+        case 3.2:
+        case 3.3:
+        case 3.4:
+        case 3.5:
+        case 3.6:
+        case 3.7:
+        case 3.8:
+        case 3.9:
+        case 4.0:
+            $data = $grade;
+            break;
+    }
+
+    return sprintf("%.1f", $data);
+}
+
      
      function getteacher($teachid)
      {
