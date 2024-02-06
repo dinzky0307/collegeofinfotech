22761742249b3fbcd5cdf18ed5c5676f095f58a0
Ryyan <132124759+Raiden-source@users.noreply.github.com>
2024-02-06 12:37:11 +0800

    Update grade.php

--- student/grade.php
+++ student/grade.php
@@ -142,11 +142,46 @@ print_r($total);
 
 function gradeconversion($grade)
 {
-    // Use number_format to ensure one decimal place is always displayed
-    return number_format($grade, 1);
+    $data = 5.0;  // Default value
+
+    switch ($grade) {
+        case 1.0:
+        case 1.1:
+        case 1.2:
+        case 1.3:
+        case 1.4:
+        case 1.5:
+        case 1.6:
+        case 1.7:
+        case 1.8:
+        case 1.9:
+        case 2.0:
+        case 2.1:
+        case 2.2:
+        case 2.3:
+        case 2.4:
+        case 2.5:
+        case 2.6:
+        case 2.7:
+        case 2.8:
+        case 2.9:
+        case 3.0:
+        case 3.1:
+        case 3.2:
+        case 3.3:
+        case 3.4:
+        case 3.5:
+        case 3.6:
+        case 3.7:
+        case 3.8:
+        case 3.9:
+        case 4.0:
+            $data = $grade;
+            break;
+    }
+
+    return sprintf("%.1f", $data);
 }
-
-
 
      
      function getteacher($teachid)
