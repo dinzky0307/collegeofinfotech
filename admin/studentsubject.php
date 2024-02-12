ddae30d600940ccd7d959ed2368f34e630ae0d10
Raiden-source <ryantibay69@gmail.com>
2024-02-12 14:08:22 +0800

    ko

--- admin/studentsubject.php
+++ admin/studentsubject.php
@@ -209,9 +209,7 @@ function gradeconversion($grade){
                 <?php
                 if (isset($grade['prelim_grade']) && isset($grade['midterm_grade']) && isset($grade['final_grade'])) {
                     $finalRatings = array_sum([
-                        $grade['prelim_grade'] * 0.3,
-                        $grade['midterm_grade'] * 0.3,
-                        $grade['final_grade'] * 0.7
+                        ((($grade['prelim_grade'] + $grade['midterm_grade']) /2) * 0.3) + (($grade['final_grade']) * 0.7)
                     ]);
                     echo $finalRatings;
                 } else {
