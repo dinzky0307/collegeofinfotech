2bb168efbd7f257bcd3e7b59a768aba0d746f570
Raiden-source <ryantibay69@gmail.com>
2024-02-12 15:13:23 +0800

    try

--- admin/myprospectus.php
+++ admin/myprospectus.php
@@ -39,81 +39,9 @@
     // $firstsem = $subject->getsubject('1st',$id);    
     // $secondsem = $subject->getsubject('2nd',$id);
     function gradeconversion($grade){
-            // $grade = round($grade);
-            if($grade==0){
-                 $data = 0;
-            }else{
-                switch ($grade) {
-                     case $grade > 1.0:
-                         $data = 1.0;
-                         break;
-                     case 1.1:
-                         $data = 1.1;
-                         break;
-                    case 1.2:
-                         $data = 1.2;
-                         break;
-                    case 1.3:
-                         $data = 1.3;
-                         break;
-                    case 1.4:
-                         $data = 1.4;
-                         break;
-                    case 1.5:
-                         $data = 1.5;
-                         break;
-                    case 1.6:
-                         $data = 1.6;
-                         break;
-                    case 1.7:
-                         $data = 1.7;
-                         break;
-                    case 1.8:
-                         $data = 1.8;
-                         break;
-                    case 1.9:
-                         $data = 1.9;
-                         break;
-                    case 2.0:
-                         $data = 2.0;
-                         break;
-                    case 2.1:
-                         $data = 2.1;
-                         break;
-                    case 2.2:
-                         $data = 2.2;
-                         break;
-                    case 2.3:
-                         $data = 2.3;
-                         break;
-                    case 2.4:
-                         $data = 2.4;
-                         break;
-                    case 2.5:
-                         $data = 2.5;
-                         break;
-                   case 2.6:
-                         $data = 2.6;
-                         break;
-                    case 2.7:
-                         $data = 2.7;
-                         break;
-                    case 2.8:
-                         $data = 2.8;
-                         break;
-                    case 2.9:
-                         $data = 2.9;
-                         break;
-                    case 3.0:
-                         $data = 3.0;
-                         break;                
-
-                     default:
-                         $data = 5.0;
-                }
-            }
-            return sprintf("%.1f",$data);
-        }
+        return number_format(round($grade, 1), 1);
+    }
+    
 
 
         $ay = $dbService->fetchRow("SELECT * from ay");
