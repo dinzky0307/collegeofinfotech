45733b8ac3a30fba488404b25d74b0597fccc8ae
Raiden-source <ryantibay69@gmail.com>
2024-02-26 14:17:43 +0800

    form adjusted

--- forgot-password.php
+++ forgot-password.php
@@ -1,5 +1,6 @@
 <?php
 include 'config.php';
+
 use PHPMailer\PHPMailer\PHPMailer;
 use PHPMailer\PHPMailer\Exception;
 use PHPMailer\PHPMailer\SMTP;
@@ -10,14 +11,14 @@ require_once("phpmailer/src/SMTP.php");
 
 if (isset($_POST['submit'])) {
     $user = $_POST['email'];
-    
+
     // Check if the provided ID number exists in the userdata table
     $query = "SELECT * FROM teacher, student WHERE email = '$user' ";
     $result = mysql_query($query);
-    
+
     if ($result && mysql_num_rows($result) == 1) {
         $data = mysql_fetch_assoc($result);
-        $verification = uniqid(rand(2,5));
+        $verification = uniqid(rand(2, 5));
         $teach_id = $data['teachid'];
         $email = $_POST['email'];
 
@@ -42,24 +43,24 @@ if (isset($_POST['submit'])) {
 
         $mail->setFrom('enelyntribunalo@gmail.com', 'MCC Info Tech');
         $mail->addAddress($email);
-        
-    
-        $session = $_SESSION['reset'] = $verification; 
-    
+
+
+        $session = $_SESSION['reset'] = $verification;
+
         //Content
         $mail->isHTML(true);                                  //Set email format to HTML
         $mail->Subject = 'Reset Password';
         $mail->Body    = "Click the link to reset password : <a href='https://infotechmcc.com/reset-pass.php?reset=$teach_id&id=$session'>Click here</a>";
-    
+
 
         $mail->send();
 
-        ?>
+?>
         <script>
             alert("Please check your email account for confirmation")
             window.location.href = "forgot-password.php"
         </script>
-        <?php 
+<?php
 
     } else {
         // User does not have an account, display an error message
@@ -69,12 +70,14 @@ if (isset($_POST['submit'])) {
 ?>
 <style>
     .btn {
-    margin-right: 20px; /* Adjust this value to control the spacing */
-    /* height: 35px;  */
-}
+        margin-right: 20px;
+        /* Adjust this value to control the spacing */
+        /* height: 35px;  */
+    }
 </style>
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
@@ -84,6 +87,7 @@ if (isset($_POST['submit'])) {
     <title>InfoTech</title>
     <link rel="stylesheet" href="css/style1.css">
 </head>
+
 <body>
     <div class="container">
         <div class="forms-container">
@@ -91,13 +95,13 @@ if (isset($_POST['submit'])) {
                 <form action="" method="post" class="sign-in-form">
                     <h2 class="title">Forgot Password</h2>
                     <div class="form-group">
-                        <?php if(isset($errorMessage)): ?>
+                        <?php if (isset($errorMessage)) : ?>
                             <p style="color: red;"><?php echo $errorMessage; ?></p>
                         <?php endif; ?>
                     </div>
                     <div class="input-field">
                         <i class="fas fa-user"></i>
-                        <input type="text" placeholder="Email" name="email"  required />
+                        <input type="text" placeholder="Email" name="email" required />
                     </div>
                     <input type="submit" value="Submit" name="submit" class="btn solid" />
                     <button type="button" class="btn cancel" id="cancelButton" style="height: 44px;">Cancel</button>
@@ -106,27 +110,28 @@ if (isset($_POST['submit'])) {
             </div>
 
 
-            <div class="panels-container" >
-      <div class="panel left-panel">
-        <div class="content">
-        <img src="img/mcc.png" alt="mcc" width="230" height="200" style="padding: 0 4px; margin-bottom: 95px; margin-right: 50px; margin-top: 30px;">
-        <p class="p" style=" text-shadow: 1px 2px 3px black;
+            <div class="panels-container">
+                <div class="panel left-panel" style="max-height: 87%">
+                    <div class="content">
+                        <img src="img/mcc.png" alt="mcc" width="230" height="200" style="padding: 0 4px; margin-bottom: 95px; margin-right: 50px; margin-top: 30px;">
+                        <p class="p" style=" text-shadow: 1px 2px 3px black;
   color: #f0f0f0;"></p>
-          <h2 class="h2" style=" text-shadow: 1px 3px 3px black;
+                        <h2 class="h2" style=" text-shadow: 1px 3px 3px black;
   color: #f0f0f0;">College of Computer Studies</h2>
+                    </div>
+                    <!-- <img src="img/register.svg" class="image" alt=""/> -->
+                </div>
+            </div>
         </div>
-        <!-- <img src="img/register.svg" class="image" alt=""/> -->
-      </div>
-    </div>
-  </div>
-  <script>
-    // Add an event listener to the Cancel button
-    document.getElementById('cancelButton').addEventListener('click', function() {
-        // Redirect the user to a cancellation page or any other desired action
-        window.location.href = 'index.php'; // Replace 'cancel-reset.php' with the desired cancellation page
-    });
-</script>
-    <script src="https://kit.fontawesome.com/64d58efce2.js" crossorigin="anonymous"></script>
-    <script src="app.js"></script>
+        <script>
+            // Add an event listener to the Cancel button
+            document.getElementById('cancelButton').addEventListener('click', function() {
+                // Redirect the user to a cancellation page or any other desired action
+                window.location.href = 'index.php'; // Replace 'cancel-reset.php' with the desired cancellation page
+            });
+        </script>
+        <script src="https://kit.fontawesome.com/64d58efce2.js" crossorigin="anonymous"></script>
+        <script src="app.js"></script>
 </body>
+
 </html>
