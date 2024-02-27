b4de418a00c635397c87f7a5d32987ddbc72230e
Raiden-source <ryantibay69@gmail.com>
2024-02-27 09:37:28 +0800

    ok

--- reset_pass.php
+++ reset_pass.php
@@ -4,7 +4,7 @@ include 'config.php';
 if (!isset($_SESSION['reset'])) {
 ?>
     <script>
-        window.location.href = "forgot_password.php"
+        window.location.href = "reset_pass.php"
     </script>
     <?php
 }
