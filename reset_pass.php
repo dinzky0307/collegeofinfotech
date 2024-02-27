feecc23420cfcf6791d68982cd631f325b17aed6
Raiden-source <ryantibay69@gmail.com>
2024-02-27 09:34:56 +0800

    changed

--- reset_pass.php
+++ reset_pass.php
@@ -2,47 +2,47 @@
 include 'config.php';
 
 if (!isset($_SESSION['reset'])) {
-    ?>
+?>
     <script>
-        window.location.href = "forgot-password.php"
+        window.location.href = "forgot_password.php"
     </script>
-    <?php 
+    <?php
 }
 
 
-if(isset($_POST['submit'])){
+if (isset($_POST['submit'])) {
     $pass = $_POST['pass'];
     $newPassword = $_POST['new_password'];
-    
+
     // Verify that the new password matches the confirmation
     if ($pass === $newPassword) {
         // Get the user's username from the session or any other method you use for identifying the user
         $user = $_GET['reset'];
-        
+
         // Generate a new password hash for the user
         $hashed = password_hash($pass, PASSWORD_DEFAULT);
-        
+
         // Update the user's password in the database
         $updateQuery = "UPDATE userdata SET password='$hashed' WHERE username='$user'";
         $result = mysql_query($updateQuery);
 
-        
+
         if ($result) {
             // Password reset successful
             $_SESSION['message'] = "";
             unset($_SESSION['reset']);
             $_SESSION['teachid'] = $user;
-            
+
             // Update the user's session data with the new password hash
             // $_SESSION['pass'] = $passHash;
 
-            ?>
+    ?>
             <script type="text/javascript">
                 alert("Password successfully updated")
                 window.location.href = "index.php"
             </script>
-            <?php 
-            
+<?php
+
             // header('location: index.php'); // Redirect to the login page
             exit();
         } else {
@@ -57,6 +57,7 @@ if(isset($_POST['submit'])){
 
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
@@ -66,6 +67,7 @@ if(isset($_POST['submit'])){
     <title>InfoTech</title>
     <link rel="stylesheet" href="css/style1.css">
 </head>
+
 <body>
     <div class="container">
         <div class="forms-container">
@@ -74,7 +76,7 @@ if(isset($_POST['submit'])){
                     <!-- Add your password reset form content here -->
                     <h2 class="title">Reset Password</h2>
                     <div class="form-group">
-                        <?php if(isset($_SESSION['message'])): ?>
+                        <?php if (isset($_SESSION['message'])) : ?>
                             <p><?php echo $_SESSION['message']; ?></p>
                             <?php unset($_SESSION['message']); ?>
                         <?php endif; ?>
@@ -92,21 +94,21 @@ if(isset($_POST['submit'])){
             </div>
 
 
-            <div class="panels-container" >
-      <div class="panel left-panel" style="max-height: 87%">
-        <div class="content">
-        <img src="img/mcc.png" alt="mcc" width="230" height="200" style="padding: 0 4px; margin-bottom: 95px; margin-right: 50px; margin-top: 30px;">
-        <p class="p" style=" text-shadow: 1px 2px 3px black;
+            <div class="panels-container">
+                <div class="panel left-panel" style="max-height: 87%">
+                    <div class="content">
+                        <img src="img/mcc.png" alt="mcc" width="230" height="200" style="padding: 0 4px; margin-bottom: 95px; margin-right: 50px; margin-top: 30px;">
+                        <p class="p" style=" text-shadow: 1px 2px 3px black;
   color: #f0f0f0;">Madridejos Community College</p>
-          <h2 class="h2" style=" text-shadow: 1px 3px 3px black;
+                        <h2 class="h2" style=" text-shadow: 1px 3px 3px black;
   color: #f0f0f0;">College of Computer</h2>
+                    </div>
+                    <!-- <img src="img/register.svg" class="image" alt=""/> -->
+                </div>
+            </div>
         </div>
-        <!-- <img src="img/register.svg" class="image" alt=""/> -->
-      </div>
-    </div>
-  </div>
-    <script src="https://kit.fontawesome.com/64d58efce2.js" crossorigin="anonymous"></script>
-    <script src="app.js"></script>
+        <script src="https://kit.fontawesome.com/64d58efce2.js" crossorigin="anonymous"></script>
+        <script src="app.js"></script>
 </body>
-</html>
 
+</html>
