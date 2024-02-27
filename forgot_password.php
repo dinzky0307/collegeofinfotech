4558df8ac22a6bdf11f866da37e910343ff4649c
Raiden-source <ryantibay69@gmail.com>
2024-02-27 14:22:35 +0800

    ok

--- forgot_password.php
+++ forgot_password.php
@@ -10,66 +10,71 @@ require_once("phpmailer/src/PHPMailer.php");
 require_once("phpmailer/src/SMTP.php");
 
 if (isset($_POST['submit'])) {
-    $username = $_POST['username'];
-    $email = $_POST['email'];
+    $user = $_POST['email'];
 
     // Check if the provided ID number exists in the userdata table
-    $query = "SELECT * FROM userdata WHERE username = '$username' AND email = '$email' ";
+    $query = "SELECT * FROM teacher WHERE email = '$user' ";
+    $student = "SELECT * FROM student WHERE email = '$user' ";
     $result = mysql_query($query);
 
-    // $result_student = mysql_query($student);
+    $result_student = mysql_query($student);
 
     if ($result && mysql_num_rows($result) == 1) {
         $data = mysql_fetch_assoc($result);
         $verification = uniqid(rand(2, 5));
-        $id = $data['id'];
-
-
-        // $email = $_POST['email'];
-
-        $mail = new PHPMailer(true);
-        $mail->SMTPDebug = 0; // Enable verbose debug output
-        $mail->isSMTP(); // Send using SMTP
-        $mail->Host = 'smtp.gmail.com'; // Set the SMTP server to send through
-        $mail->SMTPAuth = true; // Enable SMTP authentication
-        $mail->Username = 'enelyntribunalo@gmail.com'; // SMTP username
-        $mail->Password = 'tcuuujxrlvbxvdxn'; // SMTP password
-        $mail->SMTPSecure = 'tls'; // Enable TLS encryption; `PHPMailer::ENCRYPTION_SMTPS` also accepted
-        $mail->Port = 587; // TCP port to connect to
-
-        $mail->SMTPOptions = array(
-            'ssl' => array(
-                'verify_peer' => false,
-                'verify_peer_name' => false,
-                'allow_self_signed' => true
-            )
-        );
-
-        $mail->setFrom('enelyntribunalo@gmail.com', 'MCC Info Tech');
-        $mail->addAddress($email);
-
-
-        $session = $_SESSION['reset'] = $verification;
-
-        //Content
-        $mail->isHTML(true);                                  //Set email format to HTML
-        $mail->Subject = 'Reset Password';
-        $mail->Body    = "Click the link to reset password : <a href='https://infotechmcc.com/reset_pass.php?reset=$id&id=$session'>Click here</a>";
-
-
-        $mail->send();
-
-?>
-        <script>
-            alert("Please check your email account for confirmation")
-            window.location.href = "forgot_password.php"
-        </script>
-<?php
-
+        $id = $data['teachid'];
+    } else if ($result_student && mysql_num_rows($result_student) == 1) {
+        $data = mysql_fetch_assoc($result_student);
+        $verification = uniqid(rand(2, 5));
+        $id = $data['studid'];
     } else {
         // User does not have an account, display an error message
-        $errorMessage = "ID number or Email is incorrect.";
+        $errorMessage = "No account found with the provided Email account.";
     }
+    $email = $_POST['email'];
+
+    $mail = new PHPMailer(true);
+
+    $mail->SMTPDebug = 0; // Enable verbose debug output
+    $mail->isSMTP(); // Send using SMTP
+    $mail->Host = 'smtp.gmail.com'; // Set the SMTP server to send through
+    $mail->SMTPAuth = true; // Enable SMTP authentication
+    $mail->Username = 'enelyntribunalo@gmail.com'; // SMTP username
+    $mail->Password = 'tcuuujxrlvbxvdxn'; // SMTP password
+    $mail->SMTPSecure = 'tls'; // Enable TLS encryption; `PHPMailer::ENCRYPTION_SMTPS` also accepted
+    $mail->Port = 587; // TCP port to connect to
+
+    $mail->SMTPOptions = array(
+        'ssl' => array(
+            'verify_peer' => false,
+            'verify_peer_name' => false,
+            'allow_self_signed' => true
+        )
+    );
+
+    $mail->setFrom('enelyntribunalo@gmail.com', 'MCC Info Tech');
+    $mail->addAddress($email);
+
+
+    $session = $_SESSION['reset'] = $verification;
+
+    //Content
+    $mail->isHTML(true);                                  //Set email format to HTML
+    $mail->Subject = 'Reset Password';
+    $mail->Body    = "Click the link to reset password : <a href='https://infotechmcc.com/reset_pass.php?reset=$id&id=$session'>Click here</a>";
+
+
+    $mail->send();
+
+?>
+    <script>
+        alert("Please check your email account for confirmation")
+        window.location.href = "forgot_password.php"
+    </script>
+<?php
+
+
+
 }
 ?>
 <style>
@@ -102,10 +107,6 @@ if (isset($_POST['submit'])) {
                         <?php if (isset($errorMessage)) : ?>
                             <p style="color: red;"><?php echo $errorMessage; ?></p>
                         <?php endif; ?>
-                    </div>
-                    <div class="input-field">
-                        <i class="fas fa-user"></i>
-                        <input type="text" placeholder="ID number" name="username" required />
                     </div>
                     <div class="input-field">
                         <i class="fas fa-envelope"></i>
