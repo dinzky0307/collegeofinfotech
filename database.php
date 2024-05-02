0c67b158397488f3ca650e7f31a9db828b92fbec
mcces24 <158988302+mcces24@users.noreply.github.com>
2024-05-02 15:58:16 +0800

    balik

--- database.php
+++ database.php
@@ -1,18 +1,11 @@
 <?php
-$servername = "localhost";
-$username = "u510162695_infotechMCC";
-$password = "infotechMCC2023";
-$dbname = "u510162695_infotechMCC";
+session_start();
+$host = "localhost";
+$user = "u510162695_infotechMCC";
+$pass = "infotechMCC2023";
+$db = 	"u510162695_infotechMCC";
 
-// Git deployment
+mysql_connect($host, $user, $pass) or die(mysql_error());
+mysql_select_db($db);
 
-$connection = null;
-try {
-  $connection = new PDO("mysql:host=$servername;dbname=$dbname", $username, $password);
-  // set the PDO error mode to exception
-  $connection->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-
-} catch (PDOException $e) {
-  echo $sql . "<br>" . $e->getMessage();
-}
 ?>
