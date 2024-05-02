50a13030b80a5e8ad297ec68e70fbea45944641b
Raiden-source <ryantibay69@gmail.com>
2024-05-02 16:47:39 +0800

    try

--- index.php
+++ index.php
@@ -56,24 +56,25 @@ if (isset($_SESSION['level'])) {
         <form action="" method="post" class="sign-in-form">
           <h2 class="title">Log In</h2>
           <div class="form-group" style="color: red;">
-            <?php if (isset($_GET['login'])) : ?>
+            <?php if (isset($_GET['login'])): ?>
               <label class="text-danger">Invalid Username/Password try again</label>&nbsp;
             <?php endif; ?>
           </div>
           <div class="input-field">
             <i class="fas fa-user"></i>
             <input type="text" placeholder="ID number" name="user" value="<?php
-                                                                          if (isset($_SESSION['username'])) {
-                                                                            echo $_SESSION['username'];
-                                                                          }
-                                                                          ?>" required />
+            if (isset($_SESSION['username'])) {
+              echo $_SESSION['username'];
+            }
+            ?>" required />
           </div>
           <div class="input-field">
             <i class="fas fa-lock"></i>
             <input type="password" placeholder="Password" name="pass" required />
           </div>
           <input type="submit" value="Login" name="submit" class="btn solid" style="" />
-          <p style="display: flex;justify-content: center;align-items: center;margin-top: 20px;"><a href="forgot_password.php" style="color: #4590ef;">Forgot Password?</a></p>
+          <p style="display: flex;justify-content: center;align-items: center;margin-top: 20px;"><a
+              href="forgot_password.php" style="color: #4590ef;">Forgot Password?</a></p>
         </form>
       </div>
     </div>
@@ -82,11 +83,12 @@ if (isset($_SESSION['level'])) {
     <div class="panels-container">
       <div class="panel left-panel" style="max-height: 87%">
         <div class="content">
-          <img src="img/mcc.png" alt="mcc" width="230" height="200" style="padding: 0 4px; margin-bottom: 95px; margin-right: 50px; margin-top: 30px;">
+          <img src="img/mcc.png" alt="mcc" width="230" height="200"
+            style="padding: 0 4px; margin-bottom: 95px; margin-right: 50px; margin-top: 30px;">
           <p class="p" style=" text-shadow: 1px 2px 3px black;
   color: #f0f0f0;"></p>
           <h2 class="h2" style=" text-shadow: 1px 3px 3px black;
-  color: #f0f0f0;">College of Computer Studies</h2>
+  color: #f0f0f0;">College of Computer Studiess</h2>
         </div>
         <!-- <img src="img/register.svg" class="image" alt=""/> -->
       </div>
@@ -96,16 +98,20 @@ if (isset($_SESSION['level'])) {
   <script src="https://kit.fontawesome.com/64d58efce2.js" crossorigin="anonymous"></script>
   <!--   <script src="app.js"></script> -->
   <script>
-    window.addEventListener('DOMContentLoaded', (event) => {
-      function focusOnUsername() {
+    window.addEventListener('DOMContentLoaded', (event) =>
+    {
+      function focusOnUsername()
+      {
         const usernameInput = document.querySelector('input[name="user"]');
-        if (usernameInput) {
+        if (usernameInput)
+        {
           usernameInput.focus(); // Focus on the username input field
         }
       }
 
       const isMobile = /iPhone|iPad|iPod|Android/i.test(navigator.userAgent);
-      if (isMobile) {
+      if (isMobile)
+      {
         focusOnUsername(); // Call the focus function for mobile devices
       }
     });
