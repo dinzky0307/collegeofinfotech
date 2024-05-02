9f78ae59d6935e087e1ed1a4c6f253f3dba11869
Raiden-source <ryantibay69@gmail.com>
2024-05-02 17:11:07 +0800

    127

--- admin/connection.php
+++ admin/connection.php
@@ -1,18 +1,12 @@
 <?php
-$servername = "127.0.0.1";
-$username = "u510162695_infotechMCC";
-$password = "infotechMCC2023";
-$dbname = "u510162695_infotechMCC";
 
-// Git deployment
-
-$connection = null;
-try {
-    $connection = new PDO("mysql:host=$servername;dbname=$dbname", $username, $password);
-    // set the PDO error mode to exception
-    $connection->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-
-} catch (PDOException $e) {
-    echo $sql . "<br>" . $e->getMessage();
+define('DB_SERVER', '127.0.0.1');
+define('DB_USERNAME', 'u510162695_infotechMCC');
+define('DB_PASSWORD', 'infotechMCC2023');
+define('DB_NAME', 'u510162695_infotechMCC');
+$dbconnection = mysqli_connect(DB_SERVER, DB_USERNAME, DB_PASSWORD, DB_NAME);
+if ($dbconnection === false) {
+    die("ERROR: Could not connect. " . mysqli_connect_error());
 }
+
 ?>
