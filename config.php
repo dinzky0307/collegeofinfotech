6fcab0a2904c331bb3b1aac9298b4dd278264cd5
Raiden-source <ryantibay69@gmail.com>
2024-05-03 13:24:11 +0800

    try

--- config.php
+++ config.php
@@ -5,7 +5,13 @@ $user = 'u510162695_infotechMCC';
 $pass = 'infotechMCC2023';
 $db = 'u510162695_infotechMCC';
 
-mysql_connect($host, $user, $pass) or die(mysql_error());
-mysql_select_db($db);
+$connection = null;
+try {
+    $connection = new PDO("mysql:host=$servername;dbname=$dbname", $username, $password);
+    // set the PDO error mode to exception
+    $connection->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
 
+} catch (PDOException $e) {
+    echo $sql . "<br>" . $e->getMessage();
+}
 ?>
