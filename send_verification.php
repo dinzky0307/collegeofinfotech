3cbddbe2ef4c82c2635baf23125d5bcc147db4ff
dinzky0307 <64065242+dinzky0307@users.noreply.github.com>
2024-07-14 01:22:33 +0800

    Update send_verification.php

--- send_verification.php
+++ send_verification.php
@@ -23,8 +23,8 @@ $user = 'u510162695_infotechMCC';
 $pass = 'infotechMCC2023';
 $db = 'u510162695_infotechMCC';
 
-mysql_connect($host, $user, $pass) or die(mysql_error());
-mysql_select_db($db);
+//mysql_connect($host, $user, $pass) or die(mysql_error());
+//mysql_select_db($db);
     
     if ($username && filter_var($email, FILTER_VALIDATE_EMAIL)) {
         // Create a connection
