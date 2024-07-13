8a1ad07db96eaca5df426cca916096b97831f75d
dinzky0307 <64065242+dinzky0307@users.noreply.github.com>
2024-07-14 00:53:28 +0800

    Update verify_email.php

--- verify_email.php
+++ verify_email.php
@@ -1,10 +1,17 @@
 <?php
+//session_start();
+//$host = '127.0.0.1';
+//$user = 'u510162695_infotechMCC';
+//$pass = 'infotechMCC2023';
+//$db = 'u510162695_infotechMCC';
+
+//mysql_connect($host, $user, $pass) or die(mysql_error());
+//mysql_select_db($db);
+
+// Include the configuration file
+include 'config.php';
+
 // Database connection (same as in send_verification.php)
-$host = '127.0.0.1';
-$db = 'u510162695_infotechMCC';
-$user = 'u510162695_infotechMCC';
-$pass = 'your_password';
-$charset = 'utf8mb4';
 
 $dsn = "mysql:host=$host;dbname=$db;charset=$charset";
 $options = [
