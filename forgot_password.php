faf8bb6c79f7cf6d72a84c115d9cdf9399232881
townhall11 <billonesalvin69@gmail.com>
2024-11-04 20:14:10 +0800

    commit

--- forgot_password.php
+++ forgot_password.php
@@ -29,11 +29,7 @@ session_start();
             <div class="signin-signup">
             <form action="forgot-password2.php" method="post" class="sign-in-form">
     <h2 class="title">Forgot Password</h2>
-    <div class="form-group">
-        <?php if (isset($_SESSION['status'])): ?>
-            <p style="color: red;"><?php echo $_SESSION['status']; unset($_SESSION['status']); ?></p>
-        <?php endif; ?>
-    </div>
+   
     <div class="input-field">
         <i class="fas fa-envelope"></i>
         <input type="text" placeholder="Microsoft 365 Account Email" name="email" required />
