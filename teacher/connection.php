29a09b4cf90ca46ee3a20b4f16617f25652fe216
@Billones93 <115358550+townhall11@users.noreply.github.com>
2024-11-04 18:23:41 +0800

    Update connection.php

--- teacher/connection.php
+++ teacher/connection.php
@@ -1,7 +1,7 @@
 <?php
 
 define('DB_SERVER', '127.0.0.1');
-define('DB_USERNAME', 'u510162695_infotechM');
+define('DB_USERNAME', 'u510162695_infotech');
 define('DB_PASSWORD', 'infotechMCC2023');
 define('DB_NAME', 'u510162695_infotechMCC');
 $dbconnection = mysqli_connect(DB_SERVER, DB_USERNAME, DB_PASSWORD, DB_NAME);
