4c83c4f2adf8e73b300883ed341c02f0374b01b6
townhall11 <billonesalvin69@gmail.com>
2024-11-18 11:55:05 +0800

    commit

--- new_user.php
+++ new_user.php
@@ -6,7 +6,7 @@ session_start(); // Start the session
 // Check if the user is logged in
 if (!isset($_SESSION['id']) || !isset($_SESSION['level'])) {
     // If not logged in, redirect to login page
-    header('Location: login.php');
+    header('Location: index.php');
     exit();
 }
 
