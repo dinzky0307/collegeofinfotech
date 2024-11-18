b0ad054ec3131e382347c70427d6045cbc45a12f
townhall11 <billonesalvin69@gmail.com>
2024-11-18 12:27:51 +0800

    commit

--- new_user.php
+++ new_user.php
@@ -145,7 +145,7 @@ if (isset($_POST['submitEmail'])) {
                 <input type="email" name="email" id="email" placeholder="Please enter your MS 365 Email." required>
                  <div id="validationServerUsernameFeedback" class="invalid-feedback"></div>
                 <input type="submit" name="registration_link" value="Send Verification Link">
-                  <button type="button" class="btn cancel" id="cancelButton" style="height: 44px;">Cancel</button>
+                  <button type="button" class="btn cancel" id="cancelButton" style="height: 20px;">Cancel</button>
                 
             </form>
         </div>
@@ -175,5 +175,12 @@ if (isset($_POST['submitEmail'])) {
 <?php endif; ?>
 
 </body>
-
+  <script>
+            // Add an event listener to the Cancel button
+            document.getElementById('cancelButton').addEventListener('click', function ()
+            {
+                // Redirect the user to a cancellation page or any other desired action
+                window.location.href = 'index.php'; // Replace 'cancel-reset.php' with the desired cancellation page
+            });
+        </script>
 </html>
