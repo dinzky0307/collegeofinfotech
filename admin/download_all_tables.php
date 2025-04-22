6d427137f623bde37199616fa3742a533ad7a4e5
joilingford <joilingford@gmail.com>
2025-04-22 10:01:10 +0800

    asd

---
+++ admin/download_all_tables.php
@@ -0,0 +1,49 @@
+<?php
+include 'connection.php';
+
+// Set headers for file download
+header('Content-Type: application/sql');
+header('Content-Disposition: attachment; filename="database_backup_' . date('Y-m-d_H-i-s') . '.sql"');
+
+// Fetch all table names
+$tables = [];
+$result = $dbconnection->query("SHOW TABLES");
+while ($row = $result->fetch_array()) {
+    $tables[] = $row[0];
+}
+
+$backupSql = "-- Database Backup\n";
+$backupSql .= "-- Created: " . date('Y-m-d H:i:s') . "\n\n";
+$backupSql .= "SET FOREIGN_KEY_CHECKS = 0;\n\n";
+
+foreach ($tables as $table) {
+    // Drop table if exists
+    $backupSql .= "DROP TABLE IF EXISTS `$table`;\n";
+
+    // Get CREATE TABLE statement
+    $createTableResult = $dbconnection->query("SHOW CREATE TABLE `$table`");
+    $createTableRow = $createTableResult->fetch_assoc();
+    $backupSql .= $createTableRow['Create Table'] . ";\n\n";
+
+    // Get data from table
+    $dataResult = $dbconnection->query("SELECT * FROM `$table`");
+    while ($row = $dataResult->fetch_assoc()) {
+        $values = array_map([$dbconnection, 'real_escape_string'], array_values($row));
+        $values = array_map(function($val) {
+            return "'" . $val . "'";
+        }, $values);
+
+        $backupSql .= "INSERT INTO `$table` VALUES (" . implode(", ", $values) . ");\n";
+    }
+
+    $backupSql .= "\n\n";
+}
+
+$backupSql .= "SET FOREIGN_KEY_CHECKS = 1;\n";
+
+// Output the .sql content
+echo $backupSql;
+
+// Close dbconnection
+$dbconnection->close();
+?>
